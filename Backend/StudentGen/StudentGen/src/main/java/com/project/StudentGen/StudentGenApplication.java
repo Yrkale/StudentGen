726dc7ef3d6ffbc661569b3c2f758db16910ef2a
yogesh kale <ykale0660@gmail.com>
2025-11-03 12:48:08 +0530

    Create project structure

---
+++ Backend/StudentGen/StudentGen/src/main/java/com/project/StudentGen/StudentGenApplication.java
@@ -0,0 +1,13 @@
+package com.project.StudentGen;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StudentGenApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StudentGenApplication.class, args);
+	}
+
+}
---
+++ Backend/StudentGen/StudentGen/src/test/java/com/project/StudentGen/StudentGenApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project.StudentGen;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StudentGenApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
